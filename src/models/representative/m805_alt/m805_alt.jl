c4000422d36a35f87c632ab6d6c16866e7927c81
giorgiomas <74788952+giorgiomas@users.noreply.github.com>
2021-01-07 16:28:15 +0100

    Update m805_alt.jl

--- src/models/representative/m805_alt/m805_alt.jl
+++ src/models/representative/m805_alt/m805_alt.jl
@@ -543,6 +543,9 @@ function model_settings!(m::Model805_alt)
     m <= Setting(:n_anticipated_shocks, 6)
     m <= Setting(:n_anticipated_shocks_padding, 20)
 
+    # Data
+    m <= Setting(:data_id, 4, "Dataset identifier")
+
     # Conditional data variables
     m <= Setting(:cond_semi_names, [:obs_nominalrate])
     m <= Setting(:cond_full_names, [:obs_gdp, :obs_nominalrate])
