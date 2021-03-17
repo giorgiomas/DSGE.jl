00f5400b9c49eb850628ced1bef1d83e8627c06a
giorgiomas <74788952+giorgiomas@users.noreply.github.com>
2021-03-17 10:14:49 +0100

    Add a specification with larger prior mean on \sigma_c

--- src/models/representative/m1010depo/subspecs.jl
+++ src/models/representative/m1010depo/subspecs.jl
@@ -50,6 +50,8 @@ function init_subspec!(m::Model1010depo)
         return ss21!(m)
     elseif subspec(m) == "ss22"
         return ss22!(m)
+    elseif subspec(m) == "ss23"
+        return ss23!(m)
     else
         error("This subspec is not defined.")
     end
@@ -556,4 +558,41 @@ end
 function ss22!(m::Model1010depo)
     # ss20 but with inflation, long run inflation, etc. as pseudo-observables
     ss20!(m)
+end
+
+function ss23!(m::Model1010depo)
+    # ss20 + σ_c prior centered on a larger value (higher complementarity)
+
+    m <= parameter(:ρ_AAA, 0.5, (0.0, 1.0), (0.0, 1.0), ModelConstructors.SquareRoot(), BetaAlt(0.5, 0.1),
+                   fixed=false,
+                   description="ρ_AAA: AR(1) coefficient in the AAA spread process.",
+                   tex_label="\\rho_{AAA}")
+
+    m <= parameter(:ρ_BBB, 0.5, (0.0, 1.0), (0.0, 1.0), ModelConstructors.SquareRoot(), BetaAlt(0.5, 0.1),
+                   fixed=false,
+                   description="ρ_BBB: AR(1) coefficient in the BBB spread process.",
+                   tex_label="\\rho_{BBB}")
+
+    m <= parameter(:σ_BBB, 0.1, (1e-8, 5.),(1e-8, 5.),ModelConstructors.Exponential(),RootInverseGamma(2., 0.10),
+                   fixed=false,
+                   description="σ_BBB: Standard deviation on the AR(1) process for measurement error on the BBB spread.",
+                   tex_label="\\sigma_{BBB}")
+
+    m <= parameter(:σ_b_liqp, sqrt(1/400)/4, (1e-8, 5.), (1e-8, 5.), ModelConstructors.Exponential(), RootInverseGamma(100., sqrt(1/400)/4),
+                   fixed=false,
+                   description="σ_b_liqp: Standard deviation of stationary component of liquid asset preference shifter process.",
+                   tex_label="\\sigma_{b^p, liq}")
+
+    m <= parameter(:σ_b_safep, sqrt(1/400)/4, (1e-8, 5.), (1e-8, 5.), ModelConstructors.Exponential(), RootInverseGamma(100., sqrt(1/400)/4),
+                   fixed=false,
+                   description="σ_b_safep: Standard deviation of stationary component of safe asset preference shifter process.",
+                   tex_label="\\sigma_{b^p, safe}")
+
+    m <= parameter(:ρ_z_p,     0.99, (0.0, 1.0),      (0.0, 1.0), ModelConstructors.SquareRoot(),    BetaAlt(0.5, 0.2),
+                   fixed=true, description="ρ_z_p: No description available.", tex_label="\\rho_{z^p}")
+
+    m <= parameter(:σ_c, 1.8719, (1e-5, 10.), (1e-5, 10.), ModelConstructors.Exponential(), Normal(2.5, 0.37), fixed=false,
+                   description="σ_c: Coefficient of relative risk aversion.",
+                   tex_label="\\sigma_{c}")
+
 end
